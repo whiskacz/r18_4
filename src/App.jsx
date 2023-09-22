eb6028aa9dd78089ec3a57b9a3a610d4eb94746b
whiskacz <pawelblasiak1985@gmail.com>
2023-09-22 14:59:42 +0200

    r18.4 final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,36 @@
+import data from './data';
+import {useEffect, useState} from 'react';
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
+
+  const [stan, setStan] = useState(data)
+  const [inputValue, setInputValue] = ("")
+
+
+  
+console.log(data)
+ 
+useEffect(()=>{
+  const newState = stan.filter((element) => {element.name !== inputValue})
+    setStan(newState)
+},[inputValue])
+
+return (
+  <>
+  <form action="" onSubmit={(e)=> {e.preventDefault()}}></form>
+    <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)} />
+    {stan.map((element) => {
+      return (
+      <div key={element.id}>
+        <img 
+        style={{width:"50px",
+        height:"50px"}}
+        src={element.image} alt="zdjecie" />
+        {element.name}
+        {element.age}
+      </div>
+    )})}
+    <div>zbiór ma : {stan.length}</div>
+  </>
+)};
 export default App;
